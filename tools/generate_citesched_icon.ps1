595370ab2589680fd3842f29e28d3c127c69aa07
Ashhh02 <nash.cabillon@jmc.edu.ph>
2026-04-01 17:46:43 +0800

    The mobile apk icon is working

--- tools/generate_citesched_icon.ps1
+++ tools/generate_citesched_icon.ps1
@@ -48,10 +48,15 @@ if (Test-Path (Split-Path -Parent $serverBuiltLogoOutput)) {
 
 $iconTargets = @{
     'android\app\src\main\res\mipmap-mdpi\ic_launcher.png' = 48
+    'android\app\src\main\res\mipmap-mdpi\ic_launcher_round.png' = 48
     'android\app\src\main\res\mipmap-hdpi\ic_launcher.png' = 72
+    'android\app\src\main\res\mipmap-hdpi\ic_launcher_round.png' = 72
     'android\app\src\main\res\mipmap-xhdpi\ic_launcher.png' = 96
+    'android\app\src\main\res\mipmap-xhdpi\ic_launcher_round.png' = 96
     'android\app\src\main\res\mipmap-xxhdpi\ic_launcher.png' = 144
+    'android\app\src\main\res\mipmap-xxhdpi\ic_launcher_round.png' = 144
     'android\app\src\main\res\mipmap-xxxhdpi\ic_launcher.png' = 192
+    'android\app\src\main\res\mipmap-xxxhdpi\ic_launcher_round.png' = 192
     'web\favicon.png' = 32
     'web\icons\Icon-192.png' = 192
     'web\icons\Icon-maskable-192.png' = 192
